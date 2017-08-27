34003db8d38b1a32c7ec4af05d6ccb8bdcb9d459
Svb home <sergio.valverde.b@gmail.com>
2017-08-27 16:26:57 -0600

    Adding simple components

--- exercise01/src/app/app.module.ts
+++ exercise01/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './components/WarningAlert/WarningAlert.component';
+import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
 
 @NgModule({
